7754ab1fe544958f9962a8cd72f049c5e03100c4
dariossq <73315589+dariossq@users.noreply.github.com>
2021-08-28 21:30:36 -0500

    Configuració
    Se configura el Automapper y StarTup poder crear los controladores

---
+++ StoreWebApi/StoreWebApi/DTOs/AutoMapperConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using StoreWebApi.Models;
+
+namespace StoreWebApi.DTOs
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+
+                cfg.CreateMap<Noticia, NoticiaDTO>()
+                    .ReverseMap();
+
+                cfg.CreateMap<Clima, ClimaDTO>()
+                   .ReverseMap();
+
+            });
+        }
+    }
+}
